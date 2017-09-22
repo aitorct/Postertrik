f63921df801348113e2dfef086dae1a9e6789c97
Aitor Cubeles Torres <aitorct7@gmail.com>
2017-09-22 20:07:15 +0200

    Solved error when 'posts' array is empty

--- popup.js
+++ popup.js
@@ -1,22 +1,26 @@
-var posts = ["msg1", "msg2"];
+var posts = [];
 
 document.addEventListener('DOMContentLoaded', function () {
   var checkPageButton = document.getElementById('checkPage');
   checkPageButton.addEventListener('click', function () {
     chrome.tabs.getSelected(null, function (tab) {
       if (tab.url.indexOf("https://www.forocoches.com/foro/showthread.php?") !== -1) {
-        document.getElementById('status').innerHTML = "En proceso...";
-        chrome.tabs.sendMessage(tab.id, {
-          command: "postMsg",
-          title: getRandomPost()
-        }, function (msg) {
-          console.log("Result message:", msg);
-          document.getElementById('status').innerHTML = msg;
-        });
-        document.getElementById("checkPage").disabled = true;
-        setTimeout(function () {
-          document.getElementById("checkPage").disabled = false;
-        }, 30000);
+        if (posts.length > 0) {
+          document.getElementById('status').innerHTML = "En proceso...";
+          chrome.tabs.sendMessage(tab.id, {
+            command: "postMsg",
+            title: getRandomPost()
+          }, function (msg) {
+            console.log("Result message:", msg);
+            document.getElementById('status').innerHTML = msg;
+          });
+          document.getElementById("checkPage").disabled = true;
+          setTimeout(function () {
+            document.getElementById("checkPage").disabled = false;
+          }, 30000);
+        } else {
+          document.getElementById('status').innerHTML = "🔴 <b>ERROR</b><br>No hay mensajes precargados";
+        }
       } else {
         document.getElementById('status').innerHTML = "NOT FOROCOCHES";
       }
